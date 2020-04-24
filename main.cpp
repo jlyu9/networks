99df9aed9633a20ae9de1adaca9c82a3fdd5f899
Chelsy Sesma <cmsesma@ucdavis.edu>
2020-04-24 15:06:52 -0700

    initialization

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@
 #include <unistd.h>
 #include <vector>
 
+//global variables
+int length = 0;
+int time = 0;
+int service_rate = 0;
+int arrival_rate = 0;
+
+//data structures
 struct event{
 	int event_time;
 	int type;
@@ -13,7 +20,7 @@ struct event{
 
 int main(int argc, char *argv[]) {
 
-	srand48(time(NULL));
+	srand48(time(0));
 
 	//need to set up initialization
 
@@ -21,7 +28,7 @@ int main(int argc, char *argv[]) {
 	for(int i = 0; i < 100000; i++)
   {
 		//get first event from GEL
-    
+		
   }
 
 	//need to output statistics
